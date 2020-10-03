299418d3587bd6698b035af0385c00f8cc0ce118
Malikhardy23 <50418576+Malikhardy23@users.noreply.github.com>
2020-10-02 23:35:03 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("connected!")
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: 40.7128, lng: -74.0060};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 15, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+
+initMap();
